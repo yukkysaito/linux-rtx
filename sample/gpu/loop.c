308d7e09acb2095084bde421898eb18d443a6238
yukke0826 <yukke@ubi.cs.ritsumei.ac.jp>
2014-11-06 02:52:54 +0900

    update gpu sample for gpu scheduling

--- sample/gpu/loop.c
+++ sample/gpu/loop.c
@@ -12,6 +12,10 @@
 #include <stdlib.h>
 #endif
 
+/* for resch */
+#include <resch/api_gpu.h>
+#define USE_SCHED_GPU
+
 /* tvsub: ret = x - y. */
 static inline void tvsub(struct timeval *x, 
 						 struct timeval *y, 
@@ -27,6 +31,8 @@ static inline void tvsub(struct timeval *x,
 
 int cuda_test_madd(unsigned int n, char *path)
 {
+
+    struct rtxGhandle *handle;
 	int i, j, idx;
 	CUresult res;
 	CUdevice dev;
@@ -72,7 +78,7 @@ int cuda_test_madd(unsigned int n, char *path)
 	if (n % block_y != 0)
 		grid_y++;
 
-	gettimeofday(&tv_total_start, NULL);
+	
 
 	res = cuInit(0);
 	if (res != CUDA_SUCCESS) {
@@ -91,6 +97,13 @@ int cuda_test_madd(unsigned int n, char *path)
 		printf("cuCtxCreate failed: res = %lu\n", (unsigned long)res);
 		return -1;
 	}
+#ifdef USE_SCHED_GPU
+	handle = NULL;
+	rtx_gpu_init();
+	if(!(res = rtx_gpu_open(&handle, 0))){
+	    printf("rtx_gpu_open failed: res = %lu\n", (unsigned long)res);
+	}
+#endif
 
 	sprintf(fname, "%s/loop_gpu.cubin", path);
 	res = cuModuleLoad(&module, fname);
@@ -115,9 +128,6 @@ int cuda_test_madd(unsigned int n, char *path)
 		return -1;
 	}
 
-	gettimeofday(&tv_mem_alloc_start, NULL);
-
-
 	/* a[] */
 	res = cuMemAlloc(&a_dev, n*n * sizeof(unsigned int));
 	if (res != CUDA_SUCCESS) {
@@ -136,9 +146,7 @@ int cuda_test_madd(unsigned int n, char *path)
 		printf("cuMemAlloc (c) failed\n");
 		return -1;
 	}
-
-	gettimeofday(&tv_data_init_start, NULL);
-
+	
 	/* initialize A[] & B[] */
 	for (i = 0; i < n; i++) {
 		idx = i*n;
@@ -153,8 +161,6 @@ int cuda_test_madd(unsigned int n, char *path)
 		}
 	}
 
-
-	gettimeofday(&tv_h2d_start, NULL);
 	/* upload a[] and b[] */
 	res = cuMemcpyHtoD(a_dev, a, n*n * sizeof(unsigned int));
 	if (res != CUDA_SUCCESS) {
@@ -166,9 +172,6 @@ int cuda_test_madd(unsigned int n, char *path)
 		printf("cuMemcpyHtoD (b) failed: res = %lu\n", (unsigned long)res);
 		return -1;
 	}
-	gettimeofday(&tv_h2d_end, NULL);
-
-	gettimeofday(&tv_conf_kern_start, NULL);
 
 	/* set kernel parameters */
 	res = cuParamSeti(function, 0, a_dev);	
@@ -212,7 +215,9 @@ int cuda_test_madd(unsigned int n, char *path)
 		return -1;
 	}
 
-	gettimeofday(&tv_exec_start, NULL);
+#ifdef USE_SCHED_GPU
+	rtx_gpu_launch(&handle);
+#endif
 	/* launch the kernel */
 	res = cuLaunchGrid(function, grid_x, grid_y);
 	if (res != CUDA_SUCCESS) {
@@ -220,16 +225,16 @@ int cuda_test_madd(unsigned int n, char *path)
 		return -1;
 	}
 	cuCtxSynchronize();
-	gettimeofday(&tv_exec_end, NULL);
+#ifdef USE_SCHED_GPU
+	rtx_gpu_sync(&handle);
+#endif
 
-	gettimeofday(&tv_d2h_start, NULL);
 	/* download c[] */
 	res = cuMemcpyDtoH(c, c_dev, n*n * sizeof(unsigned int));
 	if (res != CUDA_SUCCESS) {
 		printf("cuMemcpyDtoH (c) failed: res = %lu\n", (unsigned long)res);
 		return -1;
 	}
-	gettimeofday(&tv_d2h_end, NULL);
 
 	/* Read back */
 	for (i = 0; i < n; i++) {
@@ -239,7 +244,6 @@ int cuda_test_madd(unsigned int n, char *path)
 		}
 	}
 
-	gettimeofday(&tv_close_start, NULL);
 
 	res = cuMemFree(a_dev);
 	if (res != CUDA_SUCCESS) {
@@ -263,6 +267,8 @@ int cuda_test_madd(unsigned int n, char *path)
 		return -1;
 	}
 
+	rtx_gpu_close(&handle);
+
 	res = cuCtxDestroy(ctx);
 	if (res != CUDA_SUCCESS) {
 		printf("cuCtxDestroy failed: res = %lu\n", (unsigned long)res);
@@ -272,9 +278,6 @@ int cuda_test_madd(unsigned int n, char *path)
 	free(a);
 	free(b);
 	free(c);
-
-	gettimeofday(&tv_total_end, NULL);
-
 
 	tvsub(&tv_mem_alloc_start, &tv_total_start, &tv);
 	init_gpu = tv.tv_sec * 1000.0 + (float) tv.tv_usec / 1000.0;
@@ -306,10 +309,6 @@ int cuda_test_madd(unsigned int n, char *path)
 	tvsub(&tv_total_end, &tv_total_start, &tv);
 	total = tv.tv_sec * 1000.0 + (float) tv.tv_usec / 1000.0;
 
-
-
-	printf("Total(ms): %f\n", total);
-	
 #if 0
 	printf("Init: %f\n", init_gpu);
 	printf("MemAlloc: %f\n", mem_alloc);
--- sample/gpu/rt_task_gpu.c
+++ sample/gpu/rt_task_gpu.c
@@ -10,8 +10,14 @@
 #include <sys/time.h>
 #include <resch/api.h>
 #include "tvops.h"
+#include <time.h>
+#include <pthread.h>
+
+#define gettime(fmt) clock_gettime(CLOCK_PROCESS_CPUTIME_ID, fmt)
 
 #define LOOP_COUNT 20
+//#define USE_SCHED_CPU
+
 
 static struct timespec ns_to_timespec(unsigned long ns)
 {
@@ -19,6 +25,18 @@ static struct timespec ns_to_timespec(unsigned long ns)
 	ts.tv_sec = ns / 1000000000;
 	ts.tv_nsec = (ns - ts.tv_sec * 1000000000LL)  ;
 	return ts;
+}
+
+static unsigned long timespec_to_ns_sub(struct timespec *ts1, struct timespec *ts2)
+{
+	unsigned long ret, ret2;
+	ret = ts1->tv_sec * 10^9;
+	ret2 = ts2->tv_sec * 10^9;
+	ret += ts1->tv_nsec;
+	ret2 += ts2->tv_nsec;
+
+	ret = ret2 - ret;
+	return ret;
 }
 
 
@@ -32,15 +50,47 @@ static struct timespec ms_to_timespec(unsigned long ms)
 
 int cuda_test_madd(unsigned int n, char *path);
 
+struct timespec period, runtime, timeout, deadline;
+
+	unsigned long prio;
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+void* thread(void *args)
+{
+    struct timespec ts1,ts2,ts3;
+    int i;  
+
+
+    gettime(&ts1);
+
+#ifdef USE_SCHED_CPU
+    rt_init(); 
+    rt_set_period(period);
+    rt_set_deadline(period);// if period = deadline, don't call "rt_set_deadline"
+    rt_set_runtime(runtime);
+
+    rt_set_scheduler(SCHED_FP); /* you can also set SCHED_EDF. */
+    rt_set_priority(prio);
+    rt_run(timeout);
+#endif
+
+    for (i = 0; i < LOOP_COUNT; i++) {
+	cuda_test_madd(1000, ".");
+    }
+    gettime(&ts2);
+
+    pthread_mutex_lock(&mutex);
+    printf("%ld,",timespec_to_ns_sub(&ts1,&ts2));
+    pthread_mutex_unlock(&mutex);
+    return NULL;
+}
 
 
 int main(int argc, char* argv[])
 {
-	int i;
-	unsigned long prio;
-	struct timespec period, runtime, timeout, deadline;
+	int i,j;
 	struct timeval tv1, tv2, tv3;
 	int dmiss_count = 0;
+	pthread_t th[4];
 
 	if (argc != 4) {
 		printf("Error: invalid option\n");
@@ -48,49 +98,29 @@ int main(int argc, char* argv[])
 		exit(EXIT_FAILURE);
 	}
 	
+#ifdef USE_SCHED_CPU
 	prio = 0;					/* priority. */
-	printf("---- period  :%d ms ----\n", atoi(argv[1]));
+	//printf("---- period  :%d ms ----\n", atoi(argv[1]));
 	period = ms_to_timespec(atoi(argv[1]));	/* period. */
-	printf("---- runtime :%d ms ----\n", atoi(argv[2]));
+	//printf("---- runtime :%d ms ----\n", atoi(argv[2]));
 	runtime = ms_to_timespec(atoi(argv[2]));			/* execution time. */
-	printf("---- deadline:%d ms ----\n", atoi(argv[3]));
+	//printf("---- deadline:%d ms ----\n", atoi(argv[3]));
 	deadline = ms_to_timespec( atoi(argv[3]) );			/* execution time. */
-	printf("---- timeout:%d ms ----\n", 30000);
+	//printf("---- timeout:%d ms ----\n", 30000);
 	timeout = ms_to_timespec(3000);			/* timeout. */
+#endif
 
-	/* bannar. */
-	printf("sample program\n");
+	for(j=0; j<4; j++)
+	    pthread_create(&th[j], NULL, thread, (void *)NULL);
+	for(j=0; j<4; j++)
+	    pthread_join(th[j],NULL);
 
-	rt_init(); 
-	rt_set_period(period);
-	rt_set_deadline(period);// if period = deadline, don't call "rt_set_deadline"
-	rt_set_runtime(runtime);
+	printf("\n");
 
-	rt_set_scheduler(SCHED_EDF); /* you can also set SCHED_EDF. */
-	rt_set_priority(prio);
-	rt_run(timeout);
-
-	for (i = 0; i < LOOP_COUNT; i++) {
-		gettimeofday(&tv1, NULL);
-		printf("start : %ld:%06ld\n", (long)tv1.tv_sec, (long)tv1.tv_usec);
-//		fflush(stdout);
-
-		cuda_test_madd(1000, ".");
-		gettimeofday(&tv2, NULL);
-		tvsub(&tv2, &tv1, &tv3);
-
-		printf("finish: %ld:%06ld\n", (long)tv2.tv_sec, (long)tv2.tv_usec);
-//		fflush(stdout);
-		if (!rt_wait_period()) {
-			printf("deadline is missed!\n");
-			dmiss_count++;
-		}
-	}
-
-	printf("Finished Tasks. %d/%d(dmiss/loopcount)\n", dmiss_count, LOOP_COUNT);
-	fflush(stdout);
-
+#ifdef USE_SCHED_CPU
 	rt_exit();
+#endif
 	
+
 	return 0;
 }
