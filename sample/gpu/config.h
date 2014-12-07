093fd2c235a45df8a31d34e51aa4ff337e29bac5
yukke0826 <yukke@ubi.cs.ritsumei.ac.jp>
2014-12-08 01:52:25 +0900

    update gpu scheduling sample. adjust to update the core and the library

---
+++ sample/gpu/config.h
@@ -0,0 +1,11 @@
+#ifndef __RTGPU_CONFIG_H__
+#define __RTGPU_CONFIG_H__
+
+//#define DISABLE_SCHED_CPU
+//#define DISABLE_SCHED_GPU
+//#define DETAIL_TIME
+//#define MUTEX_OFF
+
+#define DEFAULT_PRIO 80
+
+#endif
